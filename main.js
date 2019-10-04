b2d514ecb49b0a286ee481470e79b459c228109d
Eric Hampton <erichampton2010@gmail.com>
2019-10-04 09:20:19 -0400

    shippable

--- main.js
+++ main.js
@@ -22,16 +22,22 @@ function render_page() {
     title.innerHTML = "Calculator";
     app.appendChild(title);
 
-    var screenContainer = document.createElement('div');
-    screenContainer.setAttribute('class', 'row');
-    var screenRow = document.createElement('div');
-    screenRow.setAttribute('class', 'text-center text-white align-text-right');
-    var screenCol = document.createElement('col-12');
-    screenCol.setAttribute('class', 'text-align-right');
-    screenCol.innerHTML = "Input/Output";
-    screenRow.appendChild(screenCol);
-    screenContainer.appendChild(screenRow);
-    app.appendChild(screenContainer);
+    var display = document.createElement('div');
+    display.setAttribute('class', 'row');
+
+    var displayRow = document.createElement('div');
+    displayRow.setAttribute('class', 'text-center text-white align-text-right');
+
+    var displayCol = document.createElement('col-12');
+    displayCol.innerHTML = "Input/Output";
+    displayCol.setAttribute('class', 'text-align-right');
+    displayCol.setAttribute('id', 'display');
+
+
+
+    displayRow.appendChild(displayCol);
+    display.appendChild(displayRow);
+    app.appendChild(display);
 
     var myContainer = document.createElement('div');
     myContainer.setAttribute('class', 'container bg-dark');
@@ -76,22 +82,24 @@ function render_page() {
 
 }
 
-function calculator(operator) {
+function calculator() {
+    console.log('operator', operator)
+    console.log({ oldNum, newNum });
     switch (operator) {
         case "/":
-        return oldNum / newNum;
-        break;
+            return parseInt(newNum) / parseInt(oldNum);
+            break;
 
         case "*":
-            return oldNum * newNum;
+            return parseInt(oldNum) * parseInt(newNum);
             break;
 
         case "-":
-            return oldNum - newNume;
+            return parseInt(newNum) - parseInt(oldNum);
             break;
 
         case "+":
-            return oldNum + newNum;
+            return parseInt(oldNum) + parseInt(newNum);
             break;
 
     }
@@ -100,45 +108,76 @@ function calculator(operator) {
 
 
 function which_button() {
-    btn = (this.id);
+    btn = (this.innerHTML);
+    var displayCol = document.getElementById('display');
+    displayCol.innerHTML = btn;
+    switch (btn) {
 
-    switch(btn){
         case 'Clear':
+            console.log("Clear ", btn)
             operator = "";
             oldNum = 0;
             newNum = 0;
             screen = 0;
             count = 0;
             newInput = 0;
+            result = 0;
             break;
 
-        case '/': 
+        case '/':
         case '*':
         case '-':
         case '+':
+            console.log("operator ", btn)
 
-        if(operator != " "){
-            calculate()
-        }
+            if (operator != "") {
+                calculate()
+            }
 
-        oldNum = newInput;
-        newInput = 0;
-        operator = btn;
-        break;
+            operator = btn;
+            break;
 
         case '=':
+            console.log("=", btn)
             calculate();
+            var result = calculator();
+            console.log(result);
+
+            var displayCol = document.getElementById('display');
+            displayCol.innerHTML = result;
+
             break;
-            
-            
+
+
+        case ".":
+            console.log(". ", btn)
+            if (parseInt(oldnum) != '.') {
+
+            }
+            break;
+
+        default:
+            console.log('number:', btn)
+
+            // oldNum = newInput;
+            // newInput = 0;
+
+            if (operator != "") {
+                oldNum = btn;
+            } else {
+                newNum = btn;
+            }
+
+
+
     }
-    console.log(this.innerHTML)
 
 
 
 }
 
-function calculate(){
+function calculate() {
+
 }
 
 
